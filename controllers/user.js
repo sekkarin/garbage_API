2c8e5885e350f2f7b577f6b94e84045208b75bcd
sekkarin <sekkri1234@gmail.com>
2023-02-09 12:31:05 +0700

    ...

--- controllers/user.js
+++ controllers/user.js
@@ -87,3 +87,35 @@ exports.upDateUser = (req, res, next) => {
       });
 
 };
+exports.uploadimg = (req,res,next)=>{
+  const id_user = req.params.id
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed, entered data is incorrect.');
+    error.statusCode = 422;
+    throw error;
+  }
+  if (!req.file) {
+    const error = new Error('No image provided.');
+    error.statusCode = 422;
+    throw error;
+  }
+  const imageUrl = req.file.path.replace("\\", "/");
+  const post = new User({
+    imageUrl: imageUrl,
+  });
+  post.save()
+   .then(result => {
+    res.status(201).json({
+      message: 'Image Upload successfully!',
+      // post: post,
+      // creator: { _id: creator._id, name: creator.name }
+    });
+  })
+  .catch(err => {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  });
+}
--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,6 @@ const userController = require("../controllers/user")
 router.get("/getuser", userController.getUser)
 router.get("/getuser/:id", userController.getUserById)
 router.put("/getuser/:id", userController.upDateUser)
+router.put("/getuser/:id", userController.uploadimg)
 
 module.exports = router
